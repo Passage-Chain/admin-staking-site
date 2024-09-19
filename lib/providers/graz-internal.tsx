e3b5fafd349f88b76a9897d3f9e99efbd325fbcc
Tasio Victoria <tasiovictoria@gmail.com>
2024-09-19 16:04:48 -0400

    Improve README.md

--- lib/providers/graz-internal.tsx
+++ lib/providers/graz-internal.tsx
@@ -1,15 +1,19 @@
 import { ReactNode } from 'react'
 import { GrazProvider, WalletType } from 'graz'
 import { useChainInfos } from '@/lib/hooks/chains'
+import { useEnvVars } from './env-vars'
 
 export const GrazInternalProvider = ({ children }: { children: ReactNode }) => {
+  const envVars = useEnvVars()
   const chainInfos = useChainInfos()
+
+  const defaultWallet = envVars.development ? WalletType.LEAP : WalletType.KEPLR
 
   return (
     <GrazProvider
       grazOptions={{
         chains: chainInfos,
-        defaultWallet: WalletType.LEAP,
+        defaultWallet,
       }}
     >
       {children}
